ad829f94f7c65f45372f402bea956caaf6199a2f
Adrian E <addis.aden@gmail.com>
2014-04-01 22:12:07 +0200

    ap2 praktikum 2 fertischhhh

--- workspace/aufgabe2/src/anwendung/Main.java
+++ workspace/aufgabe2/src/anwendung/Main.java
@@ -1,12 +1,18 @@
 package anwendung;
 
+import bank.Bank;
+
 /**
  * Fuehrt beispielhaft ein paar Kontobewegungen aus
  */
 public class Main {
+	public static void ausgabe(Bank b, int k) {
+		System.out.println("Vom Konto \"" + k + "\" der Bank \"" + b.toString() +
+				"\" liegt das Saldo bei " + b.kontostand(k));
+	}
+	
     public static void main(String[] args) {
-            /* TODO:
-             *  b = neue Bank (den Namen koennen Sie selbst aussuchen)
+            /* b = neue Bank (den Namen koennen Sie selbst aussuchen)
              *
              * Erzeugen Sie ein Konto k1 und ein Konto k2 bei der Bank b
              * Zahlen Sie in k1 100 Euro ein und dann 20 Euro wieder aus.
@@ -15,5 +21,22 @@ public class Main {
              * Ueberweisen Sie 20 Euro von k1 nach k2.
              * Geben Sie die Kontobewegungen von Konto k1 und von k2 aus.
              */
+    	
+    	Bank b = new Bank("Addis Bank");
+    	int k1 = b.neuesKonto();
+    	int k2 = b.neuesKonto();
+    	
+    	b.barEinzahlen(k1, 100);
+    	b.barAuszahlen(k1, 20);
+    	
+    	ausgabe(b, k1);
+    	ausgabe(b, k2);
+    	
+    	b.barEinzahlen(k2, 70);
+    	
+    	b.ueberweisen(k1, b, k2, 20);
+    	
+    	b.kontobewegungenAusgeben(k1);
+    	b.kontobewegungenAusgeben(k2);
     }
 }
