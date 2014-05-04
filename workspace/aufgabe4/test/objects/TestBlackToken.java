f63ce8b1464c1cda82bea521b02bd0bcc3b920db
Adrian E <addis.aden@gmail.com>
2014-05-04 20:24:11 +0200

    ap2:aufgabe4 - test blacktoken ist fertig geschrieben

--- workspace/aufgabe4/test/objects/TestBlackToken.java
+++ workspace/aufgabe4/test/objects/TestBlackToken.java
@@ -1,12 +1,34 @@
 package objects;
 
-import static org.junit.Assert.fail;
+import static objects.TestExamples.*;
+import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestBlackToken {
+	private Token object;
+	
+	@Before
+	public void setUp() {
+		object = BlackToken.instance();
+	}
+	
+	@Test
+	public void isBlack() {
+		assert(object.isBlack());
+	}
+	
     @Test
-    public void toBeResolved() {
-        fail("bitte die richtigen Tests formulieren!");
+    public void unchanged() {
+    	assertSame(object, object.nextGeneration(ts2));
+    	assertSame(object, object.nextGeneration(ts3));
+    	
+    }
+    
+    @Test
+    public void changed() {
+    	assertEquals(W, object.nextGeneration(ts1));
+    	assertEquals(W, object.nextGeneration(ts4));
     }
 }
